f57e1b497074ea5f5765a26d7d6e466b44d0269c
Ann <andra.ahmad.ft22@mail.umy.ac.id>
2024-12-24 02:59:48 +0700

    menambahkan ekstensi toSuplierEntity untuk konversi ke Supplier

--- app/src/main/java/com/example/pampertemuan11/ui/viewmodel/InsertSplViewModel.kt
+++ app/src/main/java/com/example/pampertemuan11/ui/viewmodel/InsertSplViewModel.kt
@@ -1,5 +1,14 @@
 package com.example.pampertemuan11.ui.viewmodel
 
+import com.example.pampertemuan11.data.entity.Supplier
+
+
+fun SuplierEvent.toSuplierEntity(): Supplier = Supplier(
+    id = id,
+    nama = nama,
+    kontak = kontak,
+    alamat = alamat
+)
 
 data class SuplierEvent(
     val id: Int = 0,
