75bf5b0357b8fe7dd4efc8e34135467fba64f4e1
Ann <andra.ahmad.ft22@mail.umy.ac.id>
2024-12-24 02:49:42 +0700

    menambahkan class HomeSplViewModel untuk mengambil data supplier dari repository menggunakan Flow, mengelola status UI dan memberikan status UI terbaru.

--- app/src/main/java/com/example/pampertemuan11/ui/viewmodel/HomeSplViewModel.kt
+++ app/src/main/java/com/example/pampertemuan11/ui/viewmodel/HomeSplViewModel.kt
@@ -1,7 +1,51 @@
 package com.example.pampertemuan11.ui.viewmodel
 
 import androidx.core.app.NotificationCompat.MessagingStyle.Message
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.pampertemuan11.data.entity.Supplier
+import com.example.pampertemuan11.repository.RepositorySpl
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.filterNotNull
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.onStart
+import kotlinx.coroutines.flow.stateIn
+
+class HomeSplViewModel(
+    private val RepositorySpl: RepositorySpl
+): ViewModel(){
+    val homeSplUiState: StateFlow<HomeSplUiState> = RepositorySpl.getAllSpl()
+        .filterNotNull()
+        .map {
+            HomeSplUiState(
+                listSpl = it.toList(),
+                isLoading = false
+            )
+        }
+        .onStart {
+            emit(HomeSplUiState(isLoading = true))
+            delay(900)
+        }
+        .catch {
+            emit(
+                HomeSplUiState(
+                    isLoading = false,
+                    isError = true,
+                    errorMessage = it.message ?: "Terjadi Kesalahan"
+                )
+            )
+        }
+        .stateIn(
+            scope = viewModelScope,
+            started = SharingStarted.WhileSubscribed(5000),
+            initialValue = HomeSplUiState(
+                isLoading = true
+            )
+        )
+}
 
 data class HomeSplUiState(
     val listSpl: List<Supplier> = listOf(),
