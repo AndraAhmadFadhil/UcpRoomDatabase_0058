88cef2eb57b96d21199e2d805ab22611a5990e87
Ann <andra.ahmad.ft22@mail.umy.ac.id>
2024-12-24 03:42:33 +0700

    menambahkan fungsi toBarangEntity untuk mengonversi BarangEvent ke Barang entity

--- app/src/main/java/com/example/pampertemuan11/ui/viewmodel/InsertBrgViewModel.kt
+++ app/src/main/java/com/example/pampertemuan11/ui/viewmodel/InsertBrgViewModel.kt
@@ -1,6 +1,15 @@
 package com.example.pampertemuan11.ui.viewmodel
 
+import com.example.pampertemuan11.data.entity.Barang
 
+fun BarangEvent.toBarangEntity(): Barang = Barang(
+    id = id,
+    nama = nama,
+    deskripsi = deskripsi,
+    harga = harga,
+    stok = stok,
+    namaSupplier = namaSupplier,
+)
 
 data class BarangEvent(
     val id: Int = 0,
