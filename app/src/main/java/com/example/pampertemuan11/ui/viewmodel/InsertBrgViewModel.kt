60599cafd2084bf32551bbb2928005da0b1357ce
Ann <andra.ahmad.ft22@mail.umy.ac.id>
2024-12-24 03:39:18 +0700

    menambahkan data class BarangEvent untuk menyimpan data barang

---
+++ app/src/main/java/com/example/pampertemuan11/ui/viewmodel/InsertBrgViewModel.kt
@@ -0,0 +1,12 @@
+package com.example.pampertemuan11.ui.viewmodel
+
+
+
+data class BarangEvent(
+    val id: Int = 0,
+    val nama: String = "",
+    val deskripsi: String = "",
+    val harga: String = "",
+    val stok: String = "",
+    val namaSupplier: String = "",
+)
