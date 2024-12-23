2c092f2ca025de459c8dc3ab75eb0fbcd578409f
Ann <andra.ahmad.ft22@mail.umy.ac.id>
2024-12-24 03:46:46 +0700

    menambahkan FormErrorStateBar untuk validasi form barang.

--- app/src/main/java/com/example/pampertemuan11/ui/viewmodel/InsertBrgViewModel.kt
+++ app/src/main/java/com/example/pampertemuan11/ui/viewmodel/InsertBrgViewModel.kt
@@ -2,6 +2,24 @@ package com.example.pampertemuan11.ui.viewmodel
 
 import com.example.pampertemuan11.data.entity.Barang
 
+data class FormErrorStateBar(
+    val id: Int? = null,
+    val nama: String? = null,
+    val deskripsi: String? = null,
+    val harga: String? = null,
+    val stok: String? = null,
+    val namaSuplier: String? = null,
+){
+    fun isValid(): Boolean{
+        return id == null &&
+                nama == null &&
+                deskripsi == null &&
+                harga == null &&
+                stok == null &&
+                namaSuplier == null
+    }
+}
+
 fun BarangEvent.toBarangEntity(): Barang = Barang(
     id = id,
     nama = nama,
