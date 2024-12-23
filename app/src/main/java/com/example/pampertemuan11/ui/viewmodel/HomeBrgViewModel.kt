474cff7a4e10260465d48437994e761e0b58978d
Ann <andra.ahmad.ft22@mail.umy.ac.id>
2024-12-24 03:36:18 +0700

    menambahkan ViewModel untuk mengelola status UI dan pengambilan data barang

--- app/src/main/java/com/example/pampertemuan11/ui/viewmodel/HomeBrgViewModel.kt
+++ app/src/main/java/com/example/pampertemuan11/ui/viewmodel/HomeBrgViewModel.kt
@@ -1,6 +1,50 @@
 package com.example.pampertemuan11.ui.viewmodel
 
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.pampertemuan11.data.entity.Barang
+import com.example.pampertemuan11.repository.RepositoryBrg
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.filterNotNull
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.onStart
+import kotlinx.coroutines.flow.stateIn
+
+class HomeBrgViewModel(
+    private val RepositoryBrg: RepositoryBrg
+): ViewModel(){
+    val homeBrgUiState: StateFlow<HomeBrgUiState> = RepositoryBrg.getAllBrg()
+        .filterNotNull()
+        .map {
+            HomeBrgUiState(
+                listBrg = it.toList(),
+                isLoading = false,
+            )
+        }
+        .onStart {
+            emit(HomeBrgUiState(isLoading = true))
+            delay(900)
+        }
+        .catch {
+            emit(
+                HomeBrgUiState(
+                    isLoading = false,
+                    isError = true,
+                    errorMessage = it.message ?: "Terjadi Kesalahan"
+                )
+            )
+        }
+        .stateIn(
+            scope = viewModelScope,
+            started = SharingStarted.WhileSubscribed(5000),
+            initialValue = HomeBrgUiState(
+                isLoading = true
+            )
+        )
+}
 
 data class HomeBrgUiState(
     val listBrg: List<Barang> = listOf(),
