403203634a83b3cc6dcf69846ac75c168a677f34
Ann <andra.ahmad.ft22@mail.umy.ac.id>
2024-12-24 04:04:43 +0700

    menambahkan logika validasi dan penyimpanan data barang di InsertBrgViewModel.

--- app/src/main/java/com/example/pampertemuan11/ui/viewmodel/InsertBrgViewModel.kt
+++ app/src/main/java/com/example/pampertemuan11/ui/viewmodel/InsertBrgViewModel.kt
@@ -1,6 +1,67 @@
 package com.example.pampertemuan11.ui.viewmodel
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.pampertemuan11.data.entity.Barang
+import com.example.pampertemuan11.repository.RepositoryBrg
+import kotlinx.coroutines.launch
+
+class InsertBrgViewModel(private val RepositoryBarang: RepositoryBrg): ViewModel(){
+
+    var uiStateBrg by mutableStateOf(BarUIState())
+    fun updateState(barangEvent: BarangEvent){
+        uiStateBrg = uiStateBrg.copy(
+            barangEvent = barangEvent
+        )
+    }
+
+    private fun validateFields(): Boolean{
+        val event = uiStateBrg.barangEvent
+        val errorState = FormErrorStateBar(
+            nama = if (event.nama.isNotEmpty()) null else "Nama Tidak Boleh Kosong",
+            deskripsi = if (event.deskripsi.isNotEmpty()) null else "Deskripsi Tidak Boleh Kosong",
+            harga = if (event.harga.isNotEmpty()) null else "Harga Tidak Boleh Kosong",
+            stok = if (event.stok.isNotEmpty()) null else "Stok Tidak Boleh Kosong",
+            namaSuplier = if (event.namaSupplier.isNotEmpty()) null else "Nama Suplier Tidak Boleh Kosong",
+
+            )
+
+        uiStateBrg = uiStateBrg.copy(isEntrValid = errorState)
+        return errorState.isValid()
+    }
+
+    fun saveData(){
+        val currentEvent = uiStateBrg.barangEvent
+
+        if (validateFields()){
+            viewModelScope.launch {
+                try {
+                    RepositoryBarang.insertBrg(currentEvent.toBarangEntity())
+                    uiStateBrg = uiStateBrg.copy(
+                        snackbarMessage = "Data Berhasil Ditambah",
+                        barangEvent = BarangEvent(),
+                        isEntrValid = FormErrorStateBar()
+                    )
+                } catch (e: Exception){
+                    uiStateBrg = uiStateBrg.copy(
+                        snackbarMessage = "Data gagal disimpan"
+                    )
+                }
+            }
+        } else{
+            uiStateBrg = uiStateBrg.copy(
+                snackbarMessage = "Input Tidak Valid. Periksa Data Anda."
+            )
+        }
+    }
+
+    fun resetSnackBarMessage(){
+        uiStateBrg = uiStateBrg.copy(snackbarMessage = null)
+    }
+}
 
 data class BarUIState(
     val barangEvent: BarangEvent = BarangEvent(),
