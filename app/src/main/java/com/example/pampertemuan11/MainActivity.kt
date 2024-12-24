eb9c802d83c3a7ea6f91c84a75b1e187d31b46b4
Ann <andra.ahmad.ft22@mail.umy.ac.id>
2024-12-24 09:52:52 +0700

    menambahkan pengelola halaman pada main activity

--- app/src/main/java/com/example/pampertemuan11/MainActivity.kt
+++ app/src/main/java/com/example/pampertemuan11/MainActivity.kt
@@ -11,6 +11,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.pampertemuan11.ui.navigation.PengelolaHalaman
 import com.example.pampertemuan11.ui.theme.PAMPertemuan11Theme
 
 class MainActivity : ComponentActivity() {
@@ -20,8 +21,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             PAMPertemuan11Theme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+                    PengelolaHalaman(
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
